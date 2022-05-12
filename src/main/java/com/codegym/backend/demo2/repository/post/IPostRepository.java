1b587e0cb3cec6cf38ea2ac5e713c14ed5da4454
minhhieu99a <hieunb99@gmail.com>
2022-05-12 15:37:34 +0700

    delete post

--- src/main/java/com/codegym/backend/demo2/repository/post/IPostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codegym.backend.demo2.repository.post;
-
-import com.codegym.backend.demo2.model.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IPostRepository extends JpaRepository<Post,Long> {
-
-}
--- src/main/java/com/codegym/backend/demo2/service/post/IPostService.java
+++
@@ -1,6 +0,0 @@
-package com.codegym.backend.demo2.service.post;
-
-import com.codegym.backend.demo2.model.entity.Post;
-import com.codegym.backend.service.IGeneralService;
-public interface IPostService extends IGeneralService<Post> {
-}
